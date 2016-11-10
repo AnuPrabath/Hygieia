dd19e9309524e7105d2f339b38731c2ea7552aa6
Yamane <cxp284@086d41d0ed06.cof.ds.capitalone.com>
2016-11-10 16:21:24 -0600

    Templatize AppDynamics Dashboard URL

--- appdynamics-perf-collector/src/main/java/com/capitalone/dashboard/collector/DefaultAppdynamicsClient.java
+++ appdynamics-perf-collector/src/main/java/com/capitalone/dashboard/collector/DefaultAppdynamicsClient.java
@@ -201,6 +201,10 @@ public class DefaultAppdynamicsClient implements AppdynamicsClient {
         metric.setValue(callsPerMinVal * 15);
         calculatedMetrics.add(metric);
 
+        metric = new PerformanceMetric();
+        metric.setName("Dashboard URL");
+        metric.setValue(settings.getDashboardUrl());
+        calculatedMetrics.add(metric);
 
         return calculatedMetrics;
     }
